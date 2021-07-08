fdf56e81c3141c8cd393575f5a8873b9ee8f7264
cjdiniz <cjsdiniz@outlook.com>
2021-07-08 11:04:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// "use strict";
+// interface User{
+//     nome: string, email: string, telefone: string
+// }
+function enviarEmail(nome, email, telefone) {
+    console.log('Olá '+ nome + ' o seu email é ' + email + ' e o seu telefone é ' + telefone);
+}
+// enviarEmail('Carlos', 'cjsdiniz@outlook.com', '987654321');
+enviarEmail('Carlos', 'cjsdiniz@outlook.com', '987654321');
---
+++ index.ts
@@ -0,0 +1,10 @@
+// interface User{
+//     nome: string, email: string, telefone: string
+// }
+
+function enviarEmail(nome: string, email: string, telefone: string){
+    console.log('Olá ${nome} o seu email é ${email} e o seu telefone é ${telefone}');
+}
+
+// enviarEmail('Carlos', 'cjsdiniz@outlook.com', '987654321');
+enviarEmail('Carlos','cjsdiniz@outlook.com','987654321');
